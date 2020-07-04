ede8b20f6660637e7ce926effd6145cf5747e3c4
Pierre Turpin <pierre.turpin54@gmail.com>
2020-07-04 08:11:14 +0200

    Add dummy failing test

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "junit-report-annotations-action"
---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DummyTest {
+
+    @Test
+    public void failingTestToDemonstrateTheGithubAction() {
+        //noinspection SimplifiableJUnitAssertion,ConstantConditions
+        assertTrue(false);
+    }
+
+}
